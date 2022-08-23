7157ece282a7ac74d612f74b98f1aef1102dae0c
Pavel Kuznetsov <cut.in.trunks@gmail.com>
2022-08-23 22:33:17 +0500

    инициализация и настройка слайдера news

--- app/js/src/sliders.js
+++ app/js/src/sliders.js
@@ -1,14 +1,40 @@
-const swiper = new Swiper(".about-swiper", {
+const about = new Swiper(".about-swiper", {
   loop: true,
   spaceBetween: 40,
 // If we need pagination
  pagination: {
     el: ".swiper-pagination",
+    clickable: true,
 },
  // Navigation arrows
   navigation: {
     prevEl: "[data-about='prev']",
     nextEl: "[data-about='next']",
   },
+
+});
+const news = new Swiper(".news-swiper", {
+// If we need pagination
+ pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+},
+ // Navigation arrows
+  navigation: {
+    prevEl: "[data-news='prev']",
+    nextEl: "[data-news='next']",
+  },
+
+  breakpoints: {
+    320: {
+      spaceBetween: 20,
+      slidesPerView: 2.2
+    },
+    1280: {
+      spaceBetween: 45,
+      slidesPerView: 4
+
+    }
+  }
 
 });
